8346fe02537f427391c076cc68909448107b4346
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2015-11-10 20:51:14 -0200

    Documentação dos métodos de Simulator

--- src/simulator.py
+++ src/simulator.py
@@ -10,59 +10,88 @@ from threading import Thread
 class Simulator:
 
     def __init__(self, filename):
+        """Initializes the simulator's attributes."""
         self.filename = filename
         self.hosts = {}
         self.routers = {}
 
+    def start(self):
+        """Starts the simulation."""
+        # Process file
+        self.__parseFile()
+        print("<-- End of File -->")
+
+        # Begin simulation
+        for pc in self.hosts:
+            self.hosts[pc].thread = Thread(target=self.hosts[pc].runThread)
+            self.hosts[pc].thread.start()
+        # TODO: Simulate routers
+
     def createHost(self, name):
+        """Creates a host with the given name."""
         print("Host: %s" % name)
         host = Host(name)
         self.hosts[name] = host
 
     def createRouter(self, name, numInterfaces):
+        """Creates a router with the given name and number of interfaces."""
         print("Router: %s [%d interfaces]" % (name, numInterfaces))
         router = Router(name, numInterfaces)
         self.routers[name] = router
 
     def configHost(self, name, ipAddr, routerAddr, dnsAddr):
+        """Configures the host's and its default router's IP.
+           Also sets the DNS server's IP."""
         print("{IP} Host %s: %s, [Router] %s, [DNS] %s" %
               (name, ipAddr, routerAddr, dnsAddr))
         self.hosts[name].setIp(ipAddr, routerAddr, dnsAddr)
 
     def configRouter(self, name, port, ipAddr):
+        """Configures the IP address of one of the router's ports."""
         print("{IP} Router %s.%d: %s" % (name, port, ipAddr))
         self.routers[name].addPort(port, ipAddr)
 
     def createDuplexLink(self, side1, side2, bandwidth, delay):
+        """Creates a link between hosts/routers (sides 1 and 2).
+           Also configures the link's bandwidth (in Mbps) and
+           delay (in ms)."""
         print("{Link} %s <=> %s [%g Mbps, %g ms]" %
               (side1, side2, bandwidth, delay))
 
     def createRoute(self, name, subnetwork, route):
+        """Creates a specified route from a router to a subnetwork."""
         print("{Route} %s -> %s:%s" % (name, subnetwork, route))
         self.routers[name].addRoute(subnetwork, route)
 
     def defineTimePerformance(self, name, time):
+        """Sets time to process a package for a given router."""
         print("{Time} %s: %g us" % (name, time))
         self.routers[name].setTimePerformance(time)
 
     def definePortPerformance(self, name, port, bufferSize):
+        """Defines buffer size for a specified router's port."""
         print("{Buffer} %s.%d: %g" % (name, port, bufferSize))
         self.routers[name].setBufferSize(port, bufferSize)
 
     def startApplication(self, hostname, appName, appType):
+        """Defines an application level protocol on the given host."""
         print("{Application} %s: %s [%s]" % (hostname, appName, appType))
 
     def createSniffer(self, name, target, outputFile):
+        """Creates a sniffer between 'name' and 'target'. Information its
+           shown in standard output and specified 'outputFile.'"""
         # Sniffer name? Shouldn't it be link? (TODO: Check this)
         print("{Sniffer} %s <-> %s [Output '%s']" % (name, target, outputFile))
 
     def simulateCommand(self, time, appName, command):
+        """Runs 'appName' with the given command at the specified time."""
         print("{Simulate} %s %s [t = %g]" % (appName, command, time))
 
     def finish(self, time):
+        """Ends the simulation at the specified time."""
         print("**FINISH [t = %g]**" % time)
 
-    def parseFile(self):
+    def __parseFile(self):
         with open(self.filename, 'r') as simulFile:
             while True:
                 line = simulFile.readline()
@@ -173,14 +202,3 @@ class Simulator:
 
                 else:
                     print(line)
-
-    def start(self):
-        # Process file
-        self.parseFile()
-        print("<-- End of File -->")
-
-        # Begin simulation
-        for pc in self.hosts:
-            self.hosts[pc].thread = Thread(target=self.hosts[pc].runThread)
-            self.hosts[pc].thread.start()
-        # TODO: Simulate routers
